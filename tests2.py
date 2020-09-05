cb0e348014b7920724739bf7542e183894b1224d
Ram  Prakash <ramprakash@Rams-MacBook-Pro.local>
2020-09-05 10:30:16 -0700

    AES-ECB

--- tests2.py
+++ tests2.py
@@ -1,2 +1,48 @@
 #encoding with symmetric key testing
 
+import base64
+import struct
+from Crypto.Cipher import AES
+
+
+def pad16(s):
+    t = struct.pack('>I', len(s)) + s
+    return t + b'\x00' * ((16 - len(t) % 16) % 16)
+
+
+def unpad16(s):
+    n = struct.unpack('>I', s[:4])[0]
+    return s[4:n + 4]
+
+
+class Crypt(object):
+    def __init__(self, password):
+        password = pad16(password)
+        self.cipher = AES.new(password, AES.MODE_ECB)
+
+    def encrypt(self, s):
+        s = pad16(s)
+        return self.cipher.encrypt(s)
+
+    def decrypt(self, s):
+        t = self.cipher.decrypt(s)
+        return unpad16(t)
+
+
+def encrypt(s, p):
+    c = Crypt(p)
+    return c.encrypt(s)
+
+
+def decrypt(s, p):
+    c = Crypt(p)
+    return c.decrypt(s)
+
+
+msg = (b"hello loo there")
+print(msg)
+secret_key = b"keykeykey"
+
+x = encrypt(msg,secret_key)
+y = decrypt(x,secret_key)
+print(x,y)
